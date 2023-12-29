545b8d2f2dc4e6db3cac4054b0139b669a82d365
tyrrell-IH <gotothertm@gmail.com>
2023-12-30 00:00:21 +0900

    set_localeの定義箇所をbooks_controller.rbからapplication_controller.rbへ変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   def default_url_options
     { locale: I18n.locale }
   end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
