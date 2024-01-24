c840526f39228dd263a3ddc75ee4aa575e3b6c1d
tyrrell-IH <gotothertm@gmail.com>
2024-01-24 14:49:48 +0900

    deviseインストールから初期設定まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: "books#index"
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
