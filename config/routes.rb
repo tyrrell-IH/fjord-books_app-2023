b551c001e78e99b2968ee1cc7a829acf6e039ccb
tyrrell-IH <gotothertm@gmail.com>
2024-01-25 13:07:16 +0900

    ユーザリソースへのaciton及びviewファイルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "books#index"
   resources :books
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
