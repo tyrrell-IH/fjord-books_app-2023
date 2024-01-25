867505b906aa6a5ca44d9b3932ced0db8b2316a1
tyrrell-IH <gotothertm@gmail.com>
2024-01-25 14:04:54 +0900

    destroy_user_session_pathに対しdeleteメソッドが効かない問題の対応方法を変更しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
   devise_for :users
   root to: "books#index"
   resources :books
