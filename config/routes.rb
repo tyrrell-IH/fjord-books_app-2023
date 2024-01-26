c5ec06fdb8cad0efc8120e1fa052e633cc337fe6
tyrrell-IH <gotothertm@gmail.com>
2024-01-26 14:04:00 +0900

    sessionコントローラへのルーティングを変更する必要がなかったので該当箇所に修正を加えました

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
-  devise_for :users, controllers: {   registrations: 'users/registrations',
-                                                sessions: 'users/sessions' }
+  devise_for :users, controllers: {   registrations: 'users/registrations' }
   root to: "books#index"
   resources :books
   resources :users
