45df1210950cb4cf2d49eee391c00404f64e62f8
tyrrell-IH <gotothertm@gmail.com>
2024-02-20 19:24:50 +0900

    commentリソースをuserリソースの子として設計し直しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_20_051036) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_20_052037) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -54,7 +54,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_20_051036) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "reports", force: :cascade do |t|
@@ -84,5 +86,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_20_051036) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "users"
   add_foreign_key "reports", "users"
 end
