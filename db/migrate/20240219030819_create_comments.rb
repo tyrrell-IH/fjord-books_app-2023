320f9295ad81ef6738b2b47885511903e7f95308
tyrrell-IH <gotothertm@gmail.com>
2024-03-01 12:44:09 +0900

    commentsテーブルへのカラムの追加、削除、参照の追加等のためのmigrationファイルを削除し、commentsテーブル作成のmigrationファイルを直接修正

--- db/migrate/20240219030819_create_comments.rb
+++ db/migrate/20240219030819_create_comments.rb
@@ -2,6 +2,7 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.references :commentable, polymorphic: true, null: false
+      t.references :user, null: false, foreign_key: true
       t.text :body
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_20_052037) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_19_030819) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -51,10 +51,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_20_052037) do
   create_table "comments", force: :cascade do |t|
     t.string "commentable_type", null: false
     t.integer "commentable_id", null: false
+    t.integer "user_id", null: false
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
