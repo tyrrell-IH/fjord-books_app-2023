d6210e7bff3814d23db9ce7251968524405538ff
tyrrell-IH <gotothertm@gmail.com>
2024-03-01 14:09:33 +0900

    コメントが作成削除された際のリダイレクト先について、コメントの参照元のインスタンス（BookもしくはReport）を作成、そちらへリダイレクトするように設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,23 +3,25 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
+    reference_source = @comment.commentable
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment.commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human) }
+        format.html { redirect_to reference_source, notice: t('controllers.common.notice_create', name: Comment.model_name.human) }
       else
-        format.html { redirect_to @comment.commentable, alert: t('controllers.common.notice_cannot_create', name: Comment.model_name.human) }
+        format.html { redirect_to reference_source, alert: t('controllers.common.notice_cannot_create', name: Comment.model_name.human) }
       end
     end
   end
 
   def destroy
     @comment = Comment.find(params[:id])
+    reference_source = @comment.commentable
     respond_to do |format|
       if @comment.user_id == current_user.id
         @comment.destroy
-        format.html { redirect_back(fallback_location: root_path, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)) }
+        format.html { redirect_to reference_source, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human) }
       else
-        format.html { redirect_back(fallback_location: root_path, alert: t('controllers.common.notice_cannot_destroy', name: Comment.model_name.human)) }
+        format.html { redirect_to reference_source, alert: t('controllers.common.notice_cannot_destroy', name: Comment.model_name.human) }
       end
     end
   end
