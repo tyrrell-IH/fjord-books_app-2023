76b2d2ab8f048bada6d74a6b9d7e6b4cc6ffe99b
tyrrell-IH <gotothertm@gmail.com>
2024-03-02 17:26:54 +0900

    formatによ表示分けを行わないのでrespond_toの箇所を全て削除しました

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,25 +4,21 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     reference_source = @comment.commentable
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to reference_source, notice: t('controllers.common.notice_create', name: Comment.model_name.human) }
-      else
-        format.html { redirect_to reference_source, alert: t('controllers.common.notice_cannot_create', name: Comment.model_name.human) }
-      end
+    if @comment.save
+      redirect_to reference_source, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    else
+      redirect_to reference_source, alert: t('controllers.common.notice_cannot_create', name: Comment.model_name.human)
     end
   end
 
   def destroy
     @comment = Comment.find(params[:id])
     reference_source = @comment.commentable
-    respond_to do |format|
-      if @comment.user_id == current_user.id
-        @comment.destroy
-        format.html { redirect_to reference_source, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human) }
-      else
-        format.html { redirect_to reference_source, alert: t('controllers.common.notice_cannot_destroy', name: Comment.model_name.human) }
-      end
+    if @comment.user_id == current_user.id
+      @comment.destroy
+      redirect_to reference_source, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+    else
+      redirect_to reference_source, alert: t('controllers.common.notice_cannot_destroy', name: Comment.model_name.human)
     end
   end
 
