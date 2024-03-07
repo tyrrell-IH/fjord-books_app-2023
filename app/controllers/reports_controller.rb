898791ced24414fbd9f4f1cadedb784d6f79cd67
tyrrell-IH <gotothertm@gmail.com>
2024-03-07 15:41:24 +0900

    ifがネストしている箇所の可読性を上げるため、ガード節で書き直しました

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -28,14 +28,12 @@ class ReportsController < ApplicationController
   end
 
   def update
-    if @report.user_id == current_user.id
-      if @report.update(report_params)
-        redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human)
-      else
-        render :edit, status: :unprocessable_entity
-      end
+    return render :edit, status: :forbidden if @report.user_id != current_user.id
+
+    if @report.update(report_params)
+      redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
-      render :edit, status: :forbidden
+      render :edit, status: :unprocessable_entity
     end
   end
 
