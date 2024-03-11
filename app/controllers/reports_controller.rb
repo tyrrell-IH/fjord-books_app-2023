67c1a194cb40e48181f2b5955e3eff5d8336d927
tyrrell-IH <gotothertm@gmail.com>
2024-03-12 01:18:14 +0900

    日報の内容更新時にmentionsテーブルも更新されるよう修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -32,6 +32,7 @@ class ReportsController < ApplicationController
 
   def update
     if @report.update(report_params)
+      @report.update_mentioning
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit, status: :unprocessable_entity
--- app/models/mention.rb
+++ app/models/mention.rb
@@ -15,5 +15,18 @@ class Mention < ApplicationRecord
         active_mentions.create(mentioned_id: id) if Report.find_by(id:)
       end
     end
+
+    def destroy_mentioning(mentioned_ids)
+      mentioned_ids.each do |id|
+        active_mentions.find_by(mentioned_id: id).destroy!
+      end
+    end
+
+    def update_mentioning
+      mentioned_ids_in_content = search_mentioned_ids
+      mentioned_ids_already_saved = mentioning_reports.ids
+      create_mentioning(mentioned_ids_in_content - mentioned_ids_already_saved)
+      destroy_mentioning(mentioned_ids_already_saved - mentioned_ids_in_content)
+    end
   end
 end
