8d601a75c3765df3fa0c0e70001f7c337be62921
tyrrell-IH <gotothertm@gmail.com>
2024-03-13 00:40:47 +0900

    reports/destroyアクションの不要なコードを削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -43,11 +43,7 @@ class ReportsController < ApplicationController
   end
 
   def destroy
-    ActiveRecord::Base.transaction do
-      @report.mentioning_reports.destroy_all
-      @report.mentioned_reports.destroy_all
-      @report.destroy
-    end
+    @report.destroy
     redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
 
