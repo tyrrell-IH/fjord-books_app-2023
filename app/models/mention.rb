a9a3042a1f94d73509cb932b734dd99fc4ba1c09
tyrrell-IH <gotothertm@gmail.com>
2024-03-27 23:48:00 +0900

    Mentionクラスで定義したメソッド名を、より仕様が分かりやすいように変更

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -12,14 +12,14 @@ class Mention < ApplicationRecord
       self[:content].scan(URL_REGEXP).flatten.uniq.map(&:to_i)
     end
 
-    def create_mentioning(mentioned_ids)
+    def create_mentions!(mentioned_ids)
       existing_ids = Report.where(id: mentioned_ids).ids
       existing_ids.each do |id|
         active_mentions.create!(mentioned_id: id)
       end
     end
 
-    def destroy_mentioning(mentioned_ids)
+    def destroy_mentions!(mentioned_ids)
       mentioned_ids.each do |id|
         active_mentions.find_by(mentioned_id: id).destroy!
       end
@@ -29,23 +29,23 @@ class Mention < ApplicationRecord
       mentioned_ids = search_mentioned_ids
       ActiveRecord::Base.transaction do
         save
-        create_mentioning(mentioned_ids)
+        create_mentions!(mentioned_ids)
       end
     rescue ActiveRecord::RecordInvalid
       false
     end
 
-    def update_mentioning
+    def update_mentions!
       mentioned_ids_in_content = search_mentioned_ids
       mentioned_ids_already_saved = mentioning_reports.ids
-      create_mentioning(mentioned_ids_in_content - mentioned_ids_already_saved)
-      destroy_mentioning(mentioned_ids_already_saved - mentioned_ids_in_content)
+      create_mentions!(mentioned_ids_in_content - mentioned_ids_already_saved)
+      destroy_mentions!(mentioned_ids_already_saved - mentioned_ids_in_content)
     end
 
     def update_with_mentions
       ActiveRecord::Base.transaction do
         save
-        update_mentioning
+        update_mentions!
       end
     rescue ActiveRecord::RecordInvalid
       false
