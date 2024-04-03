121251433637b6c7f3a9e5de647f76f4574ede3e
tyrrell-IH <gotothertm@gmail.com>
2024-04-03 12:15:02 +0900

    Userクラスname_or_emailメソッドのテストを作成

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#name_or_email' do
+    user = User.new(email: 'tyrrell@example.com')
+    assert_equal 'tyrrell@example.com', user.name_or_email
+
+    user.name = 'tyrrell'
+    assert_equal 'tyrrell', user.name_or_email
+  end
 end
