80dae4eb31e998720faf08bd5e18c2a7ff326087
tyrrell-IH <gotothertm@gmail.com>
2024-04-03 13:31:29 +0900

    Reportクラスeditable?メソッドの単体テストを作成

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,12 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    user1 = User.create!(email: 'Alice@example.com', password: 'password')
+    user2 = User.create!(email: 'Bob@example.com', password: 'password')
+    report = user1.reports.create!(title: '初めまして', content: '初めての日報です')
+
+    assert report.editable?(user1)
+    assert_not report.editable?(user2)
+  end
 end
