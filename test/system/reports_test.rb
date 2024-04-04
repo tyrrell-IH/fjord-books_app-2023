adb1e868a573fe2f72d0a79c5fa2bc8d6c083760
tyrrell-IH <gotothertm@gmail.com>
2024-04-04 13:52:32 +0900

    Reportリソースに対するCRUDのシステムテストを作成

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -4,44 +4,52 @@ require 'application_system_test_case'
 
 class ReportsTest < ApplicationSystemTestCase
   setup do
-    @report = reports(:one)
+    @report = reports(:Alice_first_report)
+    visit root_url
+    fill_in 'Eメール', with: 'alice@example.com'
+    fill_in 'パスワード', with: 'password'
+    click_on 'ログイン'
   end
 
   test 'visiting the index' do
+    assert_text 'ログインしました。'
     visit reports_url
-    assert_selector 'h1', text: 'Reports'
+    assert_selector 'h1', text: '日報の一覧'
   end
 
   test 'should create report' do
+    assert_text 'ログインしました。'
     visit reports_url
-    click_on 'New report'
+    click_on '日報の新規作成'
 
-    fill_in 'Content', with: @report.content
-    fill_in 'Title', with: @report.title
-    fill_in 'User', with: @report.user_id
-    click_on 'Create Report'
+    fill_in 'タイトル', with: '２回目の日報です'
+    fill_in '内容', with: '２回目の日報です。だんだんプラクティスに慣れてきました。'
+    click_on '登録する'
 
-    assert_text 'Report was successfully created'
-    click_on 'Back'
+    assert_text '日報が作成されました。'
+    assert_text '２回目の日報です'
+    assert_text '２回目の日報です。だんだんプラクティスに慣れてきました。'
   end
 
   test 'should update Report' do
+    assert_text 'ログインしました。'
     visit report_url(@report)
-    click_on 'Edit this report', match: :first
+    click_on 'この日報を編集'
 
-    fill_in 'Content', with: @report.content
-    fill_in 'Title', with: @report.title
-    fill_in 'User', with: @report.user_id
-    click_on 'Update Report'
+    fill_in 'タイトル', with: 'プラクティスの進捗'
+    fill_in '内容', with: 'HTMLの課題を提出しました'
+    click_on '更新する'
 
-    assert_text 'Report was successfully updated'
-    click_on 'Back'
+    assert_text '日報が更新されました。'
+    assert_text 'プラクティスの進捗'
+    assert_text 'HTMLの課題を提出しました'
   end
 
   test 'should destroy Report' do
+    assert_text 'ログインしました。'
     visit report_url(@report)
-    click_on 'Destroy this report', match: :first
+    click_on 'この日報を削除'
 
-    assert_text 'Report was successfully destroyed'
+    assert_text '日報が削除されました。'
   end
 end
