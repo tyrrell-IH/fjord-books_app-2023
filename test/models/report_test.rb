c147e6ca23283d3ae593b362f2240ebcf8684659
tyrrell-IH <gotothertm@gmail.com>
2024-04-05 10:40:56 +0900

    テスト中で作成するユーザーのemailの値をfixtureと重複しないよう修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,8 +4,8 @@ require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
   test '#editable?' do
-    user1 = User.create!(email: 'Alice@example.com', password: 'password')
-    user2 = User.create!(email: 'Bob@example.com', password: 'password')
+    user1 = User.create!(email: 'foo@example.com', password: 'password')
+    user2 = User.create!(email: 'bar@example.com', password: 'password')
     report = user1.reports.create!(title: '初めまして', content: '初めての日報です')
 
     assert report.editable?(user1)
@@ -13,7 +13,7 @@ class ReportTest < ActiveSupport::TestCase
   end
 
   test '#created_on' do
-    user = User.create!(email: 'Alice@example.com', password: 'password')
+    user = User.create!(email: 'tyrrell@example.com', password: 'password')
     report = user.reports.create!(title: '初めまして', content: '初めての日報です', created_at: Date.new(2024, 4, 1))
     assert_equal Date.new(2024, 4, 1), report.created_on
   end
