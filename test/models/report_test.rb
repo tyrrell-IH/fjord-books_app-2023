52c01848528b5101794924950dc5f53a9e706d61
tyrrell-IH <gotothertm@gmail.com>
2024-04-15 10:26:01 +0900

    モデルテストを簡潔に書くため、fixtureを活用するように修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,8 +4,8 @@ require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
   test '#editable?' do
-    user1 = User.create!(email: 'foo@example.com', password: 'password')
-    user2 = User.create!(email: 'bar@example.com', password: 'password')
+    user1 = users(:Alice)
+    user2 = users(:Bob)
     report = user1.reports.create!(title: '初めまして', content: '初めての日報です')
 
     assert report.editable?(user1)
@@ -13,7 +13,7 @@ class ReportTest < ActiveSupport::TestCase
   end
 
   test '#created_on' do
-    user = User.create!(email: 'tyrrell@example.com', password: 'password')
+    user = users(:Alice)
     report = user.reports.create!(title: '初めまして', content: '初めての日報です', created_at: Date.new(2024, 4, 1))
 
     assert_equal Date.new(2024, 4, 1), report.created_on
