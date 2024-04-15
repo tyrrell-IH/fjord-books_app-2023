12a3f1d35297485d353bb54153fbdb949a42b846
tyrrell-IH <gotothertm@gmail.com>
2024-04-15 11:30:14 +0900

    システムテストを簡潔にするために、reportモデルのpriveteメソッドの単体テストを作成しました

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -18,4 +18,22 @@ class ReportTest < ActiveSupport::TestCase
 
     assert_equal Date.new(2024, 4, 1), report.created_on
   end
+
+  test '#save_mentions' do
+    report1 = users(:Alice).reports.create!(title: 'Aliceの日報サンプル', content: 'サンプルです')
+    report2 = users(:Bob).reports.create!(title: 'Bobの日報サンプル', content: "http://localhost:3000/reports/#{report1.id}")
+
+    assert_includes(report2.mentioning_reports, report1)
+
+    report2[:content] = 'report1を参照しない'
+    report2.save!
+
+    assert_not_includes(report2.reload.mentioning_reports, report1)
+
+    report1[:content] = "http://localhost:3000/reports/#{report1.id}"
+    report1.save!
+
+    # 自己の日報に言及できないことの確認
+    assert_not_includes(report1.mentioning_reports, report1)
+  end
 end
